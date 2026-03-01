8a2d92633a967d6077f1fdf0e32d74af04b43f56
AliceCracasso <alicecracasso@usp.br>
2026-03-01 20:20:46 -0300

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,66 @@
-// Código JS aqui!
+document.addEventListener('DOMContentLoaded', function() {
+    const isLoginPage = document.title.includes('Login');
+    
+    if (isLoginPage) {
+        const form = document.getElementById('login-form');
+        const emailInput = document.getElementById('email');
+        const passwordInput = document.getElementById('password');
+        
+        if (form && emailInput && passwordInput) {
+            form.addEventListener('submit', function(e) {
+                e.preventDefault();
+                
+                const emailCompleto = emailInput.value;
+                const usuarioNome = emailCompleto.split('@')[0];
+                
+                localStorage.setItem('usuarioNome', usuarioNome);
+                window.location.href = 'loja.html';
+            });
+        }
+    } else {
+        const nome = localStorage.getItem('usuarioNome');
+        if (nome) {
+            const spanUsuarioNome = document.getElementById('usuario-nome');
+            if (spanUsuarioNome) {
+                spanUsuarioNome.textContent = nome;
+            }
+        }
+
+
+        const logoutButton = document.getElementById('logout-button');
+        if (logoutButton) {
+            logoutButton.addEventListener('click', function() {
+                localStorage.removeItem('usuarioNome');
+                localStorage.removeItem('cartCount');
+                window.location.href = 'login.html';
+            });
+        }
+
+        let activeButtons = {
+            product1: 0,
+            product2: 0,
+            product3: 0,
+            product4: 0,
+            product5: 0,
+            product6: 0
+        };
+        function updateCartCount() {
+            const cartCount = Object.values(activeButtons).filter(value => value === 1).length;
+            localStorage.setItem('cartCount', cartCount);
+            document.getElementById('cart').textContent = cartCount;
+        }
+        for (let i = 1; i <= 6; i++) {
+            document.getElementById(`product${i}`).onclick = function() {
+                if (activeButtons[`product${i}`] === 1) {
+                    activeButtons[`product${i}`] = 0;
+                    document.getElementById(`product${i}`).textContent = 'Buy Now!'; 
+                } else {
+                    activeButtons[`product${i}`] = 1;
+                    document.getElementById(`product${i}`).textContent = 'Remove from Cart';  
+                }
+            updateCartCount();
+            };
+        };   
+}});
+
+
